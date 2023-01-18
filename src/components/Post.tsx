a1c415d2c9ec1d27b9f84622d86b862b75a1ea4e
Leonardo Pessoa <leonardopessoa799@gmail.com>
2023-01-18 12:30:27 -0300

    Add files via upload

---
+++ src/components/Post.tsx
@@ -0,0 +1,123 @@
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react';
+
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import { Avatar } from './Avatar';
+import { Comment } from './Comment';
+
+import styles from './Post.module.css';
+
+interface Author {
+  name: string;
+  role: string;
+  avatarUrl: string;
+}
+
+interface Content {
+  type: 'paragraph' | 'link';
+  content: string;
+}
+
+interface PostProps {
+  author: Author;
+  content: Content[];
+  publishedAt: Date;
+}
+
+export function Post({ author, content, publishedAt }: PostProps) {
+
+  const [comments, setComments] = useState(['Post muilo bacana, parabéns!!! 👋']);
+
+  const [newCommentText, setNewCommentText] = useState('');
+
+  const publishedAtDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:MM'h'", {
+    locale: ptBR,
+  });
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  });
+
+  function handleCreateNewComment(event: FormEvent) {
+    event.preventDefault();
+
+    setComments([...comments, newCommentText]);
+    setNewCommentText('');
+  }
+
+  function handleNewCommentChange(event: ChangeEvent<HTMLTextAreaElement>) {
+    setNewCommentText(event.target.value);
+    event.target.setCustomValidity('');
+  }
+
+  function handleNewCommentInvalid(event: InvalidEvent<HTMLTextAreaElement>) {
+    event.target.setCustomValidity('Esse campo é obrigatório!');
+  }
+
+  function deleteComment(commentToDelete: string) {
+    const commentsWithouDeletedOne = comments.filter((comment) => {
+      return comment !== commentToDelete;
+    });
+
+    setComments(commentsWithouDeletedOne);
+  }
+
+  const isNewCommentEmpty = newCommentText.length === 0;
+
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <Avatar src={author.avatarUrl} hasBorder />
+          <div className={styles.authorInfo}>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
+          </div>
+        </div>
+
+        <time title={publishedAtDateFormatted} dateTime={publishedAt.toISOString()}>{publishedDateRelativeToNow}</time>
+      </header>
+
+      <div className={styles.content}>
+        {content.map((line) => {
+          if (line.type == 'paragraph') {
+            return <p key={line.content}>{line.content}</p>
+          } else if (line.type == 'link') {
+            return <p key={line.content}><a href="#">{line.content}</a></p>
+          }
+        })}
+      </div>
+
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea
+          value={newCommentText}
+          onChange={handleNewCommentChange}
+          name='comment'
+          placeholder='Deixe um comentário'
+          onInvalid={handleNewCommentInvalid}
+          required
+        />
+
+        <footer>
+          <button type="submit" disabled={isNewCommentEmpty}>Publicar</button>
+        </footer>
+      </form>
+
+      <div className={styles.commentList}>
+        {comments.map((comment) => {
+          return (
+            <Comment
+              key={comment}
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
+          )
+        })}
+      </div>
+    </article>
+  );
+}
